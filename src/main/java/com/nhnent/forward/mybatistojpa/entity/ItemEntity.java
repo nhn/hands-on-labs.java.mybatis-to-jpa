304d071408701f9e68d57441b494768d38561343
신동민/NE <dongmyo@nhnent.com>
2018-10-02 19:54:02 +0900

    * Entity 생성

---
+++ src/main/java/com/nhnent/forward/mybatistojpa/entity/ItemEntity.java
@@ -0,0 +1,28 @@
+package com.nhnent.forward.mybatistojpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Items")
+public class ItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    private Long price;
+
+}

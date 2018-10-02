eec9617c0ab6342bc742e3644f69e292f0181a1b
신동민/NE <dongmyo@nhnent.com>
2018-10-02 20:11:27 +0900

    * 연관관계 설정

--- src/main/java/com/nhnent/forward/mybatistojpa/entity/OrderEntity.java
+++ src/main/java/com/nhnent/forward/mybatistojpa/entity/OrderEntity.java
@@ -3,15 +3,10 @@ package com.nhnent.forward.mybatistojpa.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -27,4 +22,7 @@ public class OrderEntity {
     @Column(name = "order_date")
     private Date orderDate;
 
+    @OneToMany(mappedBy = "order", cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+    List<OrderItemEntity> orderItems = new ArrayList<>();
+    
 }

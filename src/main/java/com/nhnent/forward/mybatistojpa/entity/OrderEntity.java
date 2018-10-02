49f527ff060de1a743c1a9d440d3ef3a57e720fd
신동민/NE <dongmyo@nhnent.com>
2018-10-02 19:54:02 +0900

    * Entity 생성

---
+++ src/main/java/com/nhnent/forward/mybatistojpa/entity/OrderEntity.java
@@ -0,0 +1,30 @@
+package com.nhnent.forward.mybatistojpa.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Orders")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "order_date")
+    private Date orderDate;
+
+}
---
+++ src/main/java/com/nhnent/forward/mybatistojpa/entity/OrderItemEntity.java
@@ -0,0 +1,47 @@
+package com.nhnent.forward.mybatistojpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "OrderItems")
+public class OrderItemEntity {
+    @EmbeddedId
+    private Pk pk = new Pk();
+
+    @Column
+    private Integer quantity;
+
+
+    /*
+     * Composite-id class must implement serializable.
+     * Composite-id class must define equals and hashCode methods.
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public static class Pk implements Serializable {
+        @Column(name = "order_id")
+        private Long orderId;
+
+        @Column(name = "line_number")
+        private Integer lineNumber;
+
+    }
+
+}

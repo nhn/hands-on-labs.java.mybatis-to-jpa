7bb5ea2855e6b246f73c6b6e54d39e4b440dac71
신동민/NE <dongmyo@nhnent.com>
2018-10-02 21:29:29 +0900

    * bug fix

--- src/main/java/com/nhnent/forward/mybatistojpa/controller/OrderController.java
+++ src/main/java/com/nhnent/forward/mybatistojpa/controller/OrderController.java
@@ -1,16 +1,15 @@
 package com.nhnent.forward.mybatistojpa.controller;
 
 import com.nhnent.forward.mybatistojpa.model.Order;
-import com.nhnent.forward.mybatistojpa.model.Page;
 import com.nhnent.forward.mybatistojpa.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,12 +22,8 @@ public class OrderController {
 
 
     @GetMapping("")
-    public List<Order> getOrders(@RequestParam(defaultValue = "1") int page) {
-        if (page < 1) {
-            page = 1;
-        }
-
-        return orderService.getOrders(page, Page.PAGE_SIZE);
+    public List<Order> getOrders(Pageable pageable) {
+        return orderService.getOrders(pageable);
     }
 
     @GetMapping("/{orderId}")
